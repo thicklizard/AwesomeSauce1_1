959b5c96edb3b001b2c31ed30316064a450cec31
thicklizard <thicklizard@gmail.com>
2012-08-07 16:29:24 -0500

    tweak_cache-and-cpu-freq

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -642,7 +642,7 @@ static ssize_t store_vdd_levels(struct cpufreq_policy *policy, const char *buf, 
 }
 #endif
 
-cpufreq_freq_attr_ro_perm(cpuinfo_cur_freq, 0400);
+cpufreq_freq_attr_ro(cpuinfo_cur_freq);
 cpufreq_freq_attr_ro(cpuinfo_min_freq);
 cpufreq_freq_attr_ro(cpuinfo_max_freq);
 cpufreq_freq_attr_ro(cpuinfo_transition_latency);
